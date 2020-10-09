14ead26b7275db9877916ef699df2774650c3b86
zhangweipeng <851945111@qq.com>
2020-10-09 08:57:38 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,17 @@
-$.ajaxPrefilter(function(options) {
+$.ajaxPrefilter(function(params) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    options.url = 'http://ajax.frontend.itheima.net' + options.url
+    params.url = 'http://ajax.frontend.itheima.net' + params.url
+    if (params.url.indexOf('/my/') !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    params.complete = function(res) {
+
+        var obj = res.responseJSON;
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败') {
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+        }
+    }
 })
